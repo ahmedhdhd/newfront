d04b220ab7ec04ded634d29a9e31938d0a28891f
Ziedmm <zied2012.mansour@gmail.com>
2024-08-06 19:56:22 +0200

    new front tt

--- src/app/Admin/Parametrage/Vente/Commande/add-edit-item-modal/add-edit-item-modal.component.ts
+++ src/app/Admin/Parametrage/Vente/Commande/add-edit-item-modal/add-edit-item-modal.component.ts
@@ -1,12 +1,95 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { DétailsCommande } from '../../../../../Models/DétailsCommande';
+import { Product } from '../../../../../Models/product';
+import { environment } from '../../../../../../environments/environment';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { map, Observable, startWith } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-add-edit-item-modal',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './add-edit-item-modal.component.html',
   styleUrl: './add-edit-item-modal.component.css'
 })
-export class AddEditItemModalComponent {
+export class AddEditItemModalComponent implements OnInit {
+  @Output() itemAdded = new EventEmitter<DétailsCommande>();
+  @Input() item?: any;
+  baseUrl = environment.apiUrl;
 
-}
+  itemForm: FormGroup;
+  productCtrl = new FormControl();
+  filteredProducts: Observable<Product[]>;
+  products: Product[] = [];
+  selectedProduct: Product | undefined;
+
+  constructor(private fb: FormBuilder, private http: HttpClient) {
+    this.itemForm = this.fb.group({
+      name: [''],
+      Qte: ['']
+    });
+
+    this.filteredProducts = this.productCtrl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filterProducts(value || ''))
+    );
+  }
+
+  ngOnInit(): void {
+    this.loadProducts();
+    if (this.item) {
+      this.patchItem();
+    }
+  }
+
+  private loadProducts(): void {
+    this.http.get<Product[]>(`${this.baseUrl}/Produit/all`).subscribe(
+      (products: Product[]) => {
+        this.products = Array.isArray(products) ? products : [];
+      },
+      error => {
+        console.error('Failed to load products:', error);
+        this.products = [];
+      }
+    );
+  }
+
+  private _filterProducts(value: string): Product[] {
+    const filterValue = value.toLowerCase();
+    return this.products.filter(product => product.name.toLowerCase().includes(filterValue));
+  }
+
+  onProductSelected(event: any): void {
+    const selectedProductName = event.option.value;
+    this.selectedProduct = this.products.find(product => product.name === selectedProductName);
+    if (this.selectedProduct) {
+      this.itemForm.patchValue({ name: this.selectedProduct.name });
+    }
+  }
+
+  patchItem(): void {
+    if (this.item) {
+      this.itemForm.patchValue({
+        name: this.item.idProduit,
+        Qte: this.item.qte
+      });
+      this.selectedProduct = this.products.find(product => product.id === this.item?.idProduit);
+    }
+  }
+
+  addItem(): void {
+    const formValue = this.itemForm.value;
+    if (this.selectedProduct) {
+      const detailsCommande: DétailsCommande = {
+        qte: formValue.Qte,
+        prixUnitaire: this.selectedProduct.prixTTC,
+        prixTotal: formValue.Qte * this.selectedProduct.prixTTC,
+        idProduit: this.selectedProduct.id,
+        id: this.item ? this.item.id : 0
+      };
+      console.log('Item added:', detailsCommande);
+      this.itemAdded.emit(detailsCommande);
+    }
+  }}
--- src/app/Admin/Parametrage/Vente/Commande/details-commande/details-commande.component.ts
+++ src/app/Admin/Parametrage/Vente/Commande/details-commande/details-commande.component.ts
@@ -1,12 +1,106 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DetailsCommandeService } from '../../../../../Services/details-commande.service';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { CommandeService } from '../../../../../Services/commande.service';
+import { DétailsCommande } from '../../../../../Models/DétailsCommande';
+import { AddEditItemModalComponent } from '../add-edit-item-modal/add-edit-item-modal.component';
+import { MatDialog } from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TextInputComponent } from '../../../../../text-input/text-input.component';
 
 @Component({
   selector: 'app-details-commande',
   standalone: true,
-  imports: [],
+  imports: [TextInputComponent,FormsModule,CommonModule,RouterModule ,ReactiveFormsModule ],
   templateUrl: './details-commande.component.html',
   styleUrl: './details-commande.component.css'
 })
-export class DetailsCommandeComponent {
+export class DetailsCommandeComponent implements OnInit {
+  commande?: any;
+  ide: any;
 
+  constructor(
+    private DetailsCommandeService: DetailsCommandeService,
+    private CommandeService: CommandeService,
+
+    private route: ActivatedRoute,
+    private dialog: MatDialog ) {  }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.ide = parseInt(id, 10);
+      this.getOrderDetails(this.ide);
+    }
+  }
+
+  getOrderDetails(id: number) {
+    this.CommandeService.getCommandebyid(id.toString()).subscribe({
+      next: commande => {
+        console.log(commande);
+        this.commande = commande;
+      },
+      error: err => console.error(err)
+    });
+  }
+
+  deleteitems(commandid: number, itemid: number) {
+    this.DetailsCommandeService.deleteitems(commandid, itemid).subscribe({
+      next: () => {
+        this.getOrderDetails(this.ide);
+      },
+      error: err => console.error(err)
+    });
+  }
+
+  additem(commandid: number, item: DétailsCommande) {
+    this.DetailsCommandeService.addItemToCommande(commandid, item).subscribe({
+      next: () => {
+        this.getOrderDetails(this.ide);
+      },
+      error: err => console.error(err)
+    });
+  }
+
+  updateItem(commandid: number, itemid: number, item: DétailsCommande) {
+    this.DetailsCommandeService.updateItem(commandid, itemid, item).subscribe({
+      next: () => {
+        this.getOrderDetails(this.ide);
+      },
+      error: err => console.error(err)
+    });
+  }
+
+  openAddItemModal() {
+    const dialogRef = this.dialog.open(AddEditItemModalComponent);
+
+    dialogRef.componentInstance.itemAdded.subscribe((item: DétailsCommande) => {
+      this.handleItemAdded(item);
+    });
+
+    dialogRef.afterClosed().subscribe(() => {
+    });
+  }
+
+  openEditItemModal(item: DétailsCommande , id : number) {
+    const dialogRef = this.dialog.open(AddEditItemModalComponent, {
+      data: item 
+    });
+
+    dialogRef.componentInstance.itemAdded.subscribe((updatedItem: DétailsCommande) => {
+      this.handleItemUpdated(updatedItem , id);
+    });
+
+    dialogRef.afterClosed().subscribe(() => {
+    });
+  }
+
+  handleItemAdded(item: DétailsCommande) {
+    this.additem(this.ide, item);
+  }
+
+  handleItemUpdated(item: DétailsCommande , id : number) {
+    this.updateItem(this.ide, id , item);
+  }
 }
